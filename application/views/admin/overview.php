68c067a120f81eeb570e1fac4408876a5780016e
fauziardha1 <fauziardasaputra@gmail.com>
2020-01-07 22:53:47 +0700

    inisiasi project dan menambahkan contoh template sederhana

---
+++ application/views/admin/overview.php
@@ -0,0 +1,15 @@
+        <?php $this->load->view('_partials/head.php'); 
+
+         $this->load->view('_partials/sidebar.php'); 
+
+         $this->load->view('_partials/navbar.php');
+
+        //  $this->load->view('_partials/content.php');
+
+         $this->load->view('_partials/footer.php'); 
+
+         $this->load->view('_partials/scrollTop.php'); 
+
+         $this->load->view('_partials/logoutModal.php');  
+
+        $this->load->view('_partials/script.php');?>
